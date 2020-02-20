15d30998683bd5547e3eddde588ab21c5c389cec
Jonathan <59838557+JHardeman@users.noreply.github.com>
2020-02-20 14:49:15 -0500

    Review Controller Tests Done

--- src/test/java/org/wecancodeit/reviews/controllers/ReviewControllerTest.java
+++ src/test/java/org/wecancodeit/reviews/controllers/ReviewControllerTest.java
@@ -2,6 +2,9 @@ package org.wecancodeit.reviews.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.ui.Model;
 import org.wecancodeit.reviews.models.Category;
 import org.wecancodeit.reviews.models.Hashtag;
@@ -9,8 +12,8 @@ import org.wecancodeit.reviews.models.Review;
 import org.wecancodeit.reviews.storage.ReviewStorage;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ReviewControllerTest {
 
@@ -25,17 +28,36 @@ public class ReviewControllerTest {
         mockStorage = mock(ReviewStorage.class);
         underTest = new ReviewController(mockStorage);
         model = mock(Model.class);
-        Category testCategory = new Category("Sedam");
+        Category testCategory = new Category("Sedan");
         Hashtag testHashtag = new Hashtag("");
         testReview = new Review(testCategory, "I Really Love This Car OMG", "Great Review");
-        when(mockStorage.findReviewById(1L)).thenReturn(testReview);
+        when(mockStorage.findReviewById(1)).thenReturn(testReview);
 
     }
 
     @Test
     public void displayReviewReturnsReviewTemplate(){
-        String result = underTest.displayReview(1L, model);
+        String result = underTest.displayReview(1, model);
         assertThat(result).isEqualTo("review-view");
+
+    }
+
+    @Test
+    public void displayReviewInteractsWithDependenciesCorrectly(){
+
+        underTest.displayReview(1, model);
+        verify(mockStorage).findReviewById(1);
+        verify(model).addAttribute("review", testReview);
+    }
+
+    @Test
+    public void displayReviewMappingIsCorrect() throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/review/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("review-view"))
+                .andExpect(model().attributeExists("review"))
+                .andExpect(model().attribute("review", testReview));
 
     }
 
