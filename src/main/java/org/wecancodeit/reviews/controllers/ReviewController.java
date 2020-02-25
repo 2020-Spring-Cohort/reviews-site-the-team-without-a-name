54dd966ed46f13226642c38cf727c915c0a05c2e
Mercy <mercybaning@gmail.com>
2020-02-25 11:05:42 -0500

    added review form

--- src/main/java/org/wecancodeit/reviews/controllers/ReviewController.java
+++ src/main/java/org/wecancodeit/reviews/controllers/ReviewController.java
@@ -3,9 +3,15 @@ package org.wecancodeit.reviews.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.wecancodeit.reviews.models.Category;
+import org.wecancodeit.reviews.models.Hashtag;
 import org.wecancodeit.reviews.models.Review;
 import org.wecancodeit.reviews.storage.ReviewStorage;
+
+import java.awt.print.Book;
 
 
 @Controller
@@ -25,5 +31,11 @@ public class ReviewController {
         model.addAttribute("review", retrievedReview);
         return "review-view";
 
+    }
+
+    @PostMapping("/add-review")
+    public String addReview(@RequestParam Category category, String title, String reviewBody, Hashtag... hashtag) {
+        reviewStorage.store(new Review(category, title, reviewBody, hashtag));
+        return "redirect:review";
     }
 }
