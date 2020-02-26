66fecab7ffacf9b33267abaa6187028e9fa536f1
Jonathan <59838557+JHardeman@users.noreply.github.com>
2020-02-26 15:03:51 -0500

    cannot add reviews yet

--- src/main/java/org/wecancodeit/reviews/controllers/CategoryController.java
+++ src/main/java/org/wecancodeit/reviews/controllers/CategoryController.java
@@ -7,12 +7,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.wecancodeit.reviews.models.Category;
+import org.wecancodeit.reviews.models.Comment;
+import org.wecancodeit.reviews.models.Hashtag;
+import org.wecancodeit.reviews.models.Review;
 import org.wecancodeit.reviews.storage.CategoryStorage;
+import org.wecancodeit.reviews.storage.ReviewStorage;
+import org.wecancodeit.reviews.storage.repositories.CategoryRepository;
+import org.wecancodeit.reviews.storage.repositories.ReviewRepository;
+
 
 
 @Controller
 public class CategoryController {
     private CategoryStorage categoryStorage;
+    private ReviewRepository reviewRepo;
+    private ReviewStorage reviewStorage;
+    private CategoryRepository categoryRepo;
 
     public CategoryController(CategoryStorage categoryStorage) {
         this.categoryStorage = categoryStorage;
@@ -37,5 +47,17 @@ public class CategoryController {
     public String addCategory(@RequestParam String type) {
         categoryStorage.store(new Category(type));
         return "redirect:categories";
+    }
+
+    @PostMapping("/categories/{categoryType}/add-review")
+    public String addReviewToCategory(@PathVariable String categoryType,
+                                      @RequestParam String category, String reviewTitle, String reviewBody){
+        Category categoryToAddReviewTo= categoryStorage.findCategoryByType(category.getType());
+        Review reviewToAddToCategory = new Review(inputCategory, reviewTitle, reviewBody);
+        reviewRepo.save(reviewToAddToCategory);
+        categoryToAddReviewTo.addReviewToCategory(reviewToAddToCategory);
+        categoryStorage.store(categoryToAddReviewTo);
+        return "redirect:/categories/" + categoryType;
+
     }
 }
